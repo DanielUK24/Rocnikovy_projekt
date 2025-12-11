b348f529a22d5813c45ea0c4547b449a61c542ea
DanielUK24 <jason.hilak@gmail.com>
2025-12-11 09:16:30 +0100

    add_missing_lines but not tested

--- project_root/src/transform/Transformer.py
+++ project_root/src/transform/Transformer.py
@@ -1,5 +1,7 @@
 from src.extract.CSVExtractor import CSVExtractor
 from dateutil import parser
+from dateinfer import infer
+
 
 class Trasformer:
     def __init__(self, csv_file_path):
@@ -23,6 +25,54 @@ class Trasformer:
                     prev = act
             print(dif_count)
             return max(dif_count, key=dif_count.get)
+    
+    def _create_middle_line(self, line1, line2):
+        middle_line = {}
+        for key in line1:
+                # to do: genericity
+                if key == "Beach Name":
+                      middle_line[key] = line1[key]
+                # to do: genericity
+                if key == "Measurement Timestamp":
+                      middle_line[key] = ""
+                else:
+                    if line1[key] == "" and line2[key] == "":
+                          middle_line[key] = ""
+                    if line1[key] != "" and line2[key] != "":
+                         middle_line[key] =  round( ( float(line1[key]) + float(line2[key]) )/2 , 3 )
+        return middle_line
+                      
+    
+    def _add_missing_lines(self, lines, common_difference):
+          
+          # to do: implement to .json and constructor
+          maximum_time_difference = 24
+          
+          act = lines[0]["Measurement Timestamp"]
+          new_lines = [act]
+
+          for line in lines:
+            next = line["Measurement Timestamp"]
+            dif = self._time_difference(act, next)
+
+            if dif <= common_difference or maximum_time_difference < dif:
+                    new_lines.append(line)
+                    act = line        
+                    continue
+
+            middle_line = self._create_middle_line(act, next)
+            act_time = parser.parse(act["Measurement Timestamp"])
+            number_of_missing_lines = dif%common_difference
+
+            for j in range(number_of_missing_lines):
+                  middle_line["Measurement Timestamp"] = act_time + j*common_difference
+                  new_lines.append(middle_line)
+
+            new_lines.append(next)            
+            prev = act
+
+            return new_lines
+
 
     def apply_trans_all(self, lines):
          
